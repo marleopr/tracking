318cfaea1b2ad973788cf2a6cce21a7342fd424b
Márleo Piber da Rosa <marleopr@hotmail.com>
2023-07-24 13:52:11 -0300

    Adicionada função de apagar itens do historico

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,12 +2,14 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BASE_URL, token, user } from '../constants/BASE_URL';
 import TrackingInfo from './TrackingInfo';
+import { FaTrash } from 'react-icons/fa';
 
 const HomePage = () => {
     const [apiData, setApiData] = useState(null);
     const [codigo, setCodigo] = useState("");
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
+
     // Função auxiliar para recuperar o histórico do localStorage
     const getSearchHistoryFromLocalStorage = () => {
         const savedHistory = localStorage.getItem('searchHistory');
@@ -15,11 +17,20 @@ const HomePage = () => {
     };
     const [searchHistory, setSearchHistory] = useState(getSearchHistoryFromLocalStorage());
 
+    const handleDeleteSearch = (indexToDelete) => {
+        setSearchHistory((prevHistory) => {
+            const updatedHistory = prevHistory.filter((item, index) => index !== indexToDelete)
+            localStorage.setItem('searchHistory', JSON.stringify(updatedHistory))
+            return updatedHistory
+        })
+    }
 
     // Salvar o histórico no localStorage sempre que for atualizado
     useEffect(() => {
         localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
     }, [searchHistory]);
+
+
 
     const getTracks = async () => {
         setError(null);
@@ -46,6 +57,24 @@ const HomePage = () => {
         await getTracks();
     };
 
+    // Função para realizar a pesquisa sempre que o estado `codigo` for atualizado
+    useEffect(() => {
+        if (codigo.trim() !== '') {
+            handleSearch(); // Chama a função de pesquisa novamente
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [codigo]);
+
+    // Função para realizar uma nova pesquisa com o código clicado no histórico
+    const handleSearchFromHistory = (codigo) => {
+        setCodigo(codigo); // Define o código para realizar a pesquisa
+        setError(null); // Limpa o estado de erro
+        setApiData(null); // Limpa o estado de dados da API
+        handleSearch(); // Chama a função de pesquisa novamente
+    };
+
+
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <div>
@@ -61,13 +90,20 @@ const HomePage = () => {
                         <TrackingInfo trackingData={apiData} />
                     </div>
                 ) : (
-                    // Exibe o histórico de pesquisas apenas quando não há pesquisa em andamento e nenhum resultado disponível
                     searchHistory.length > 0 && (
                         <div style={{ marginTop: '20px', textAlign: 'center' }}>
                             <h2>Histórico de Pesquisas:</h2>
-                            <ul style={{ listStyle: 'none', padding: 0 }}>
+                            <ul style={{ listStyle: 'none', padding: 0, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
                                 {searchHistory.map((item, index) => (
-                                    <li key={index}>{item}</li>
+                                    <li key={index} style={{ display: 'flex', alignItems: 'center', marginBottom: '5px' }}>
+                                        <span onClick={() => handleSearchFromHistory(item)} style={{ cursor: 'pointer' }}>
+                                            {item}
+                                        </span>
+                                        <FaTrash
+                                            style={{ marginLeft: '5px', cursor: 'pointer' }}
+                                            onClick={() => handleDeleteSearch(index)}
+                                        />
+                                    </li>
                                 ))}
                             </ul>
                         </div>
@@ -77,5 +113,4 @@ const HomePage = () => {
         </div>
     );
 };
-
 export default HomePage;
